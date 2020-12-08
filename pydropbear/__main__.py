4cca6b49126aed8996b5f2f774d27fa12950744a
u1234x1234 <u1234x1234@gmail.com>
2020-12-09 00:02:05 +0500

    Update __main__.py

--- pydropbear/__main__.py
+++ pydropbear/__main__.py
@@ -5,7 +5,7 @@ from .server import start_ssh_server
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--port", type=int, default=8443)
-    arg_parser.add_argument("--bg", type=bool, default=False)
+    arg_parser.add_argument("--bg", type=int, default=0)
     args = arg_parser.parse_args()
 
     start_ssh_server(port=args.port, background=args.bg)
